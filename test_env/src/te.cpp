1f61133ce7cd594ce13f428a1e0dd7178bdb9997
Darrik Mazey <darrik@darmasoft.com>
2011-01-16 11:36:44 -0500

    working test environment again

--- test_env/src/te.cpp
+++ test_env/src/te.cpp
@@ -1,15 +1,28 @@
 
 #include "libgesture.h"
 #include <iostream>
+#include <opencv.hpp>
 
 int main(int argc, char **argv)
 {
 	GestureContext *ctx = LibGesture::init();
 
 	while(1) {
-		static int c = 0;
-		std::cout << "iteration " << c++ << std::endl;
-		sleep(5);
+		char k = -1;
+		k = cvWaitKey(10);
+		if (k == 27) {
+			break;
+		}
+		RGBImage *rgbImage = ctx->lastRGBImage();
+		DepthImage *depthImage = ctx->lastDepthImage();
+		if (rgbImage) {
+			cv::imshow("rgb", (*(rgbImage->cvMat())));
+			cvMoveWindow("rgb", 0, 0);
+		}
+		if (depthImage) {
+			cv::imshow("depth", (*(depthImage->heatMap().cvMat())));
+			cvMoveWindow("depth", 640, 0);
+		}
 	}
 
 	delete(ctx);
